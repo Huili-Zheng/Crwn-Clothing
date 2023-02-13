28472b2bd1cc78d9c9acbd7beccba9ff993bf5af
Huili Zheng <hz2710@cumc.columbia.edu>
2023-02-12 21:23:17 -0500

    reselect library to create memoized selectors

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,6 +1,23 @@
-export const selectCategoriesMap = (state) =>
-  state.categories.categories.reduce((acc, category) => {
-    const { title, items } = category;
-    acc[title.toLowerCase()] = items;
-    return acc;
-  }, {});
+import { createSelector } from "reselect";
+
+//  selector that selects the categories slice of the state from the store
+const selectCategoryReducer = (state) => {
+  return state.categories;
+};
+
+// Selector that selects the array of categories from the categories slice of the state
+export const selectCategories = createSelector(
+  [selectCategoryReducer],
+  (categoriesSlice) => categoriesSlice.categories
+);
+
+// Selector that maps the array of categories to an object with lowercased titles as keys and items as values
+export const selectCategoriesMap = createSelector(
+  [selectCategories],
+  (categories) =>
+    categories.reduce((acc, category) => {
+      const { title, items } = category;
+      acc[title.toLowerCase()] = items;
+      return acc;
+    }, {})
+);
