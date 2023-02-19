bab1033d5b2f26675aa57b4e25668071ca8a45ec
Huili Zheng <hz2710@cumc.columbia.edu>
2023-02-19 11:45:37 -0500

    typing directory item

--- src/components/directory-item/directory-item.component.tsx
+++ src/components/directory-item/directory-item.component.tsx
@@ -3,9 +3,17 @@ import {
   Body,
   BackgroundImage,
 } from "./directory-item.styles";
-import { useNavigate } from "react-router-dom";
 
-const DirectoryItem = ({ category }) => {
+import { CategoryDescription } from "../../store/categories/category.types";
+
+import { useNavigate } from "react-router-dom";
+import { FC } from "react";
+
+export type DirectoryItemProps = {
+  category: CategoryDescription;
+};
+
+const DirectoryItem: FC<DirectoryItemProps> = ({ category }) => {
   const { imageUrl, title } = category;
   const navigate = useNavigate();
   const goToCategoryHandler = () => {
