94b1a19a9ef77328f69799a9f98fc2e6571cfcbc
Huili Zheng <hz2710@cumc.columbia.edu>
2023-02-19 10:18:46 -0500

    declare the module and include it inside the TSconfig to type SVG in React

--- src/components/cart-icon/cart-icon.component.tsx
+++ src/components/cart-icon/cart-icon.component.tsx
@@ -7,11 +7,9 @@ import {
 } from "../../store/cart/cart.selector";
 import { setIsCartOpen } from "../../store/cart/cart.action";
 
-import {
-  ShoppingIcon,
-  CartIconContainer,
-  ItemCount,
-} from "./cart-icon.styles.jsx";
+import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
+
+import { CartIconContainer, ItemCount } from "./cart-icon.styles.jsx";
 
 const CartIcon = () => {
   const dispatch = useDispatch();
