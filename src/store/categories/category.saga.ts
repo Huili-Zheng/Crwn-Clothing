cebfc2f93f8604b24b637e364d6303a2db532077
Huili Zheng <hz2710@cumc.columbia.edu>
2023-02-18 21:22:48 -0500

    typing category saga

--- src/store/categories/category.saga.ts
+++ src/store/categories/category.saga.ts
@@ -1,4 +1,4 @@
-import { takeLatest, call, all, put } from "redux-saga/effects";
+import { takeLatest, call, all, put } from "typed-redux-saga";
 
 import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
 import {
@@ -10,10 +10,10 @@ import { CATEGORIES_ACTION_TYPES } from "./category.types";
 
 export function* fetchCategoriesAsync() {
   try {
-    const categoriesArray = yield call(getCategoriesAndDocuments, "categories");
-    yield put(fetchCategoriesSuccess(categoriesArray));
+    const categoriesArray = yield* call(getCategoriesAndDocuments);
+    yield* put(fetchCategoriesSuccess(categoriesArray));
   } catch (error) {
-    yield put(fetchCategoriesFailed(error));
+    yield* put(fetchCategoriesFailed(error as Error));
   }
 }
 
